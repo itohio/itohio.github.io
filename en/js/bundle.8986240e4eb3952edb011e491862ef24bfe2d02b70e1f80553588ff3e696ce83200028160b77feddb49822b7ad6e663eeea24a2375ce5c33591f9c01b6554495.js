3e8f059afcd6082c913bc5a37b656648d754d119
foxis <foxis@users.noreply.github.com>
2022-11-12 23:04:18 +0000

    deploy: cc15446d866610429857d0eaa9dda01c091e21b8

--- en/js/bundle.8986240e4eb3952edb011e491862ef24bfe2d02b70e1f80553588ff3e696ce83200028160b77feddb49822b7ad6e663eeea24a2375ce5c33591f9c01b6554495.js
+++ en/js/bundle.8986240e4eb3952edb011e491862ef24bfe2d02b70e1f80553588ff3e696ce83200028160b77feddb49822b7ad6e663eeea24a2375ce5c33591f9c01b6554495.js
@@ -8,7 +8,7 @@ const scaleImageClass = 'image-scale';
 const pageHasLoaded = 'DOMContentLoaded';
 const imageAltClass = 'img_alt'
 
-const baseURL = 'https://itohio.github.io/';
+const baseURL = 'https://itohi.com/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
